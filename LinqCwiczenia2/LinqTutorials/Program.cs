9b8319288dc5aa5460ed51db3fe9a55300a2f0d2
Gaspa <35897552+Gaspa123@users.noreply.github.com>
2021-08-01 20:50:45 +0200

    Add files via upload

---
+++ LinqCwiczenia2/LinqTutorials/Program.cs
@@ -0,0 +1,19 @@
+﻿using LinqTutorials.Models;
+using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task12();
+
+            foreach (Emp d in t)
+            {
+                Console.WriteLine(d.Empno);
+            }
+
+        }
+    }
+}
